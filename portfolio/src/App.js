f3d787ff57ccbf94d6e463770153e7d50fcbca06
Christophe <christophalpha@yahoo.com>
2020-11-01 03:05:15 +0100

    first mockup with CRA create react app and bootstrap components mixed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './components/sidebar'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Timeline from './components/timeline'
+//import Blog from './components/blog'
+import Skills from './components/skills'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div id="colorlib-page">
+				<div id="container-wrap">
+					<Sidebar />
+					<div id="colorlib-main">
+						<Introduction></Introduction>
+						<About />
+						<Skills />
+						<Timeline />
+						{ /*<Blog></Blog> */}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ portfolio/src/components/timeline.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>IT specialist<span> 2000 - 2015</span></h2>
+                        <p>parag text 1.</p>
+                        <p>parag text 2.</p>
+                          </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Front End Web Developer<span> 2019-2020</span></h2>
+                        <p>parag text 1.</p>
+                        <p>parag text 2.</p>
+                          </div>
+                    </div>
+                  </article>
+                 {/*  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Graduation and Higher Education <span>2009- Jul 2011</span></h2>
+                        <p>Engineering (Computer Science): 2011-2015<br />
+						Higher Secondary Education: 2009-2011<br />
+						Senior Secondary Education: 2009</p> 						
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
